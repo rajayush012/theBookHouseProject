77b4c3e2a4a5657dd76392d9e633589c872f7d49
Ayush Raj <raj.ayush012@gmail.com>
2018-12-11 13:02:34 +0530

    Book app is up

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Books(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    detail = models.TextField()
+
+    def shortSum(self):
+        return self.detail[:150]
+
+    def __str__(self):
+        return self.title
